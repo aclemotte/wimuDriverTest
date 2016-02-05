7b0425317e7304e42d3288ca8da8870028053dde
Miguel <velasch@gmail.com>
2016-02-05 12:33:13 +0100

    Proyecto de lectura de la DLL IMU-GNEC

---
+++ IMU-gNeC-test/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace IMU_gNeC_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonCalibrate = new System.Windows.Forms.Button();
+            this.buttonRotate = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.buttonSetUp = new System.Windows.Forms.Button();
+            this.buttonInitGame = new System.Windows.Forms.Button();
+            this.buttonEndGame = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonConnect.Location = new System.Drawing.Point(16, 18);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(190, 44);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDisconnect.Location = new System.Drawing.Point(16, 68);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(190, 44);
+            this.buttonDisconnect.TabIndex = 1;
+            this.buttonDisconnect.Text = "Disconnect";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(280, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(280, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(280, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // buttonCalibrate
+            // 
+            this.buttonCalibrate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCalibrate.Location = new System.Drawing.Point(16, 119);
+            this.buttonCalibrate.Name = "buttonCalibrate";
+            this.buttonCalibrate.Size = new System.Drawing.Size(92, 44);
+            this.buttonCalibrate.TabIndex = 5;
+            this.buttonCalibrate.Text = "Calibrate";
+            this.buttonCalibrate.UseVisualStyleBackColor = true;
+            this.buttonCalibrate.Click += new System.EventHandler(this.buttonCalibrate_Click);
+            // 
+            // buttonRotate
+            // 
+            this.buttonRotate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRotate.Location = new System.Drawing.Point(114, 119);
+            this.buttonRotate.Name = "buttonRotate";
+            this.buttonRotate.Size = new System.Drawing.Size(92, 44);
+            this.buttonRotate.TabIndex = 6;
+            this.buttonRotate.Text = "Rotate";
+            this.buttonRotate.UseVisualStyleBackColor = true;
+            this.buttonRotate.Click += new System.EventHandler(this.buttonRotate_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(259, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "R";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(259, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "P";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(259, 39);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 20);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Y";
+            // 
+            // buttonSetUp
+            // 
+            this.buttonSetUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSetUp.Location = new System.Drawing.Point(67, 203);
+            this.buttonSetUp.Name = "buttonSetUp";
+            this.buttonSetUp.Size = new System.Drawing.Size(82, 44);
+            this.buttonSetUp.TabIndex = 10;
+            this.buttonSetUp.Text = "SetUp";
+            this.buttonSetUp.UseVisualStyleBackColor = true;
+            this.buttonSetUp.Click += new System.EventHandler(this.buttonSetUp_Click);
+            // 
+            // buttonInitGame
+            // 
+            this.buttonInitGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonInitGame.Location = new System.Drawing.Point(16, 269);
+            this.buttonInitGame.Name = "buttonInitGame";
+            this.buttonInitGame.Size = new System.Drawing.Size(190, 44);
+            this.buttonInitGame.TabIndex = 11;
+            this.buttonInitGame.Text = "InitGame";
+            this.buttonInitGame.UseVisualStyleBackColor = true;
+            this.buttonInitGame.Click += new System.EventHandler(this.buttonInitGame_Click);
+            // 
+            // buttonEndGame
+            // 
+            this.buttonEndGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEndGame.Location = new System.Drawing.Point(16, 319);
+            this.buttonEndGame.Name = "buttonEndGame";
+            this.buttonEndGame.Size = new System.Drawing.Size(190, 44);
+            this.buttonEndGame.TabIndex = 12;
+            this.buttonEndGame.Text = "EndGame";
+            this.buttonEndGame.UseVisualStyleBackColor = true;
+            this.buttonEndGame.Click += new System.EventHandler(this.buttonEndGame_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(271, 307);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 20);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Main angle";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(271, 331);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 20);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(181, 193);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(14, 20);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "-";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(181, 227);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(14, 20);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 379);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.buttonEndGame);
+            this.Controls.Add(this.buttonInitGame);
+            this.Controls.Add(this.buttonSetUp);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.buttonRotate);
+            this.Controls.Add(this.buttonCalibrate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.buttonConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonDisconnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonCalibrate;
+        private System.Windows.Forms.Button buttonRotate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button buttonSetUp;
+        private System.Windows.Forms.Button buttonInitGame;
+        private System.Windows.Forms.Button buttonEndGame;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ IMU-gNeC-test/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+//using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using IMU_gNeC;
+
+namespace IMU_gNeC_test
+{
+    public partial class Form1 : Form
+    {
+        #region attributes
+
+            IMUClass myIMU = new IMUClass();
+            int step = 0;
+
+        #endregion
+
+        #region methods
+
+            public Form1()
+            {
+                //myIMU.initializeThreadIMU();
+
+                myIMU.IMUAng.ImuYPR += new IMUEventHandler(ImuYPRreceived);
+                myIMU.imuOR.ImuOR += new ImuOREventHandler(ImuORreceived);
+                myIMU.imuPerm.ImuPerm += new ImuPermEventHandler(ImuPermReceived);
+                myIMU.imuROM.ImuROM += new ImuROMEventHandler(ImuRomOKReceived);
+
+                InitializeComponent();
+            }
+
+            
+            private void buttonConnect_Click(object sender, EventArgs e)
+            {
+                bool connection1, connection2;
+                if (step == 0)
+                {
+                    try
+                    {
+                        myIMU.initializeSerialPort();
+                        step++;
+                    }
+                    catch (Exception exc) { };
+                }
+                if (step == 1)
+                {
+                    connection1 = myIMU.connectIMUkownCOM("COM118", 1);
+                    if (!connection1)
+                        step++;
+                    else
+                        myIMU.startReadingIMU();
+
+                }
+                if (step == 2)
+                {
+                    connection2 = myIMU.connectIMUunkownCOM(1);
+                    if (connection2)
+                        myIMU.startReadingIMU();
+                }
+            }
+
+            private void buttonDisconnect_Click(object sender, EventArgs e)
+            {
+                myIMU.disconnectIMU();
+                Application.Exit();
+            }
+                
+            private void buttonCalibrate_Click(object sender, EventArgs e)
+            {
+                myIMU.ImuConfigPar.Calibrate = true;
+                myIMU.ImuConfigPar.Rotate = false;
+                myIMU.ImuConfigPar.SetUp = false;
+                myIMU.ImuConfigPar.SendSetUp();
+            }
+        
+            private void buttonRotate_Click(object sender, EventArgs e)
+            {
+                myIMU.ImuConfigPar.Calibrate = false;
+                myIMU.ImuConfigPar.Rotate = true;
+                myIMU.ImuConfigPar.SetUp = false;
+                myIMU.ImuConfigPar.SendSetUp();
+            }
+
+            private void buttonSetUp_Click(object sender, EventArgs e)
+            {
+                myIMU.ImuConfigPar.Calibrate = false;
+                myIMU.ImuConfigPar.Rotate = false;
+                myIMU.ImuConfigPar.SetUp = true;
+                myIMU.ImuConfigPar.MainAngleROMrange = 30;
+                myIMU.ImuConfigPar.MainAngleROMangle = "Y";
+                myIMU.ImuConfigPar.SecundaryAngle1ROMrange = 10;
+                myIMU.ImuConfigPar.SecundaryAngle1ROMangle = "P";
+                myIMU.ImuConfigPar.SecundaryAngle2ROMrange = 10;
+                myIMU.ImuConfigPar.SecundaryAngle2ROMangle = "R";
+                myIMU.ImuConfigPar.TimePerm = 1;
+                myIMU.ImuConfigPar.ThetaPerm = 10;
+                myIMU.ImuConfigPar.SendSetUp();
+            }
+
+            private void buttonInitGame_Click(object sender, EventArgs e)
+            {
+                myIMU.ImuInitgame.InitGame = true;
+                myIMU.ImuInitgame.SendInitEndGame();
+            }
+
+            private void buttonEndGame_Click(object sender, EventArgs e)
+            {
+                myIMU.ImuInitgame.InitGame = false;
+                myIMU.ImuInitgame.SendInitEndGame();
+            }
+
+            
+            public void ImuYPRreceived(object sender, IMUEventArgs e)
+            {
+                Console.WriteLine("Yaw " + Math.Floor(e.Yaw).ToString()
+                                 + ", Pitch " + Math.Floor(e.Pitch).ToString()
+                                 + ", Roll " + Math.Floor(e.Roll).ToString());
+
+                WriteLabel(label1, Math.Floor(e.Yaw).ToString());
+                WriteLabel(label2, Math.Floor(e.Pitch).ToString());
+                WriteLabel(label3, Math.Floor(e.Roll).ToString());
+            }
+
+            public void ImuORreceived(object sender, ImuOREventArgs e)
+            {
+                 WriteLabel(label8, Math.Floor(e.MainAngle).ToString());
+                 WriteLabel(label9, "-");
+            }
+            
+            public void ImuPermReceived(object sender, ImuPermEventArgs e)
+            {
+                WriteLabel(label9, "PERMANENCIA");
+                Console.WriteLine("Permanencia en ángulo: " + Math.Floor(e.Alpha).ToString());
+            }
+
+            public void ImuRomOKReceived(object sender, ImuROMEventArgs e)
+            {               
+                if (e.RomOK)
+                {
+                    Console.WriteLine("En rango");
+                    WriteLabel(label10, "En rango");
+                }
+                else
+                {
+                    Console.WriteLine("FUERA DE RANGO");
+                    WriteLabel(label10, "FUERA DE RANGO");
+                }
+            }
+
+            
+            delegate void WriteLabelDelegate(Label label, string text);
+            private void WriteLabel(Label label, string text)
+            {
+                if (label.InvokeRequired)
+                    label.Invoke(new WriteLabelDelegate(this.WriteLabel), new object[] { label, text });
+                else
+                {
+                    label.Text = text;
+                }
+            }
+        
+        #endregion    
+    }
+}
